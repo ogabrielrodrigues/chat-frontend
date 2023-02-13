4f8617c68b3f8839fa49d5405959094d61350777
ogabrielrodrigues <gabrielrodrigues.dev409@gmail.com>
2023-02-13 19:10:47 -0300

    feat: added message sent

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,8 +1,65 @@
 import "./styles.css";
-
+import { v4 as uuid } from "uuid";
 import { X, PaperPlaneRight } from "phosphor-react";
+import { io } from "../../lib/socketio";
+import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
+
+import { Message, MessageProps } from "../../components/Message";
+
+interface User {
+  id: string;
+  username: string;
+  status: boolean;
+}
 
 export function Chat() {
+  const { current: socket } = useRef(io.connect());
+  const [messages, setMessages] = useState<MessageProps[]>([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [user, setUser] = useState<User>({
+    id: uuid().split("-")[0],
+    status: true,
+    username: `test-${uuid().split("-")[0]}`,
+  });
+
+  useEffect(() => {
+    const onMessageReceived = (message: MessageProps) => {
+      setMessages((messages) => [...messages, message]);
+    };
+
+    socket.on("ready", console.log);
+
+    socket.on("joined_on_room", (room: string) => setRoom(room));
+
+    socket.on("replies", onMessageReceived);
+
+    return () => {
+      socket.off("replies", onMessageReceived);
+    };
+  }, [socket]);
+
+  const sendMessage = useCallback(() => {
+    const [hrs, scs] = new Date().toLocaleTimeString().split(":");
+
+    socket.emit("new_message", {
+      authorId: user.id,
+      username: user.username,
+      content: newMessage,
+      sendedAt: [hrs, scs].join(":"),
+    });
+
+    setNewMessage("");
+  }, [socket, newMessage]);
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if (newMessage.trim() === "") return;
+
+    sendMessage();
+  }
+
   return (
     <div className="app">
       <div className="top">
@@ -12,8 +69,10 @@ export function Chat() {
             alt="User image"
           />
           <div className="user-status">
-            <strong>User name</strong>
-            <div className="status">Online</div>
+            <strong>{user.username}</strong>
+            <div className={`status ${user.status ? "online" : "offline"}`}>
+              {user.status ? "Online" : "Offline"}
+            </div>
           </div>
         </div>
 
@@ -25,49 +84,26 @@ export function Chat() {
         <div className="last-seen">Today - 11:30AM</div>
 
         <div className="messages">
-          <div className="message">
-            <div className="message-top">Cecilia - 11:30AM</div>
-            <div className="message-body">Teste de implemetação!</div>
-          </div>
-
-          <div className="message you">
-            <div className="message-top">You - 11:32AM</div>
-            <div className="message-body">Wow</div>
-          </div>
-
-          <div className="message">
-            <div className="message-top">Cecilia - 11:34AM</div>
-            <div className="message-body">Try now</div>
-          </div>
-
-          <div className="message you">
-            <div className="message-top">You - 11:38AM</div>
-            <div className="message-body">Let's start</div>
-          </div>
-          <div className="message">
-            <div className="message-top">Cecilia - 11:30AM</div>
-            <div className="message-body">Teste de implemetação!</div>
-          </div>
-
-          <div className="message you">
-            <div className="message-top">You - 11:32AM</div>
-            <div className="message-body">Wow</div>
-          </div>
-
-          <div className="message">
-            <div className="message-top">Cecilia - 11:34AM</div>
-            <div className="message-body">Try now</div>
-          </div>
-
-          <div className="message you">
-            <div className="message-top">You - 11:38AM</div>
-            <div className="message-body">Let's start</div>
-          </div>
+          {messages.map((message, i) => (
+            <Message
+              authorId={message.authorId}
+              content={message.content}
+              username={message.username}
+              sendedAt={message.sendedAt}
+              loggedUser={user.id}
+              key={`${i}-${message.authorId}`}
+            />
+          ))}
         </div>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="input-wrapper">
-          <input type="text" placeholder="Type your message" />
+          <input
+            type="text"
+            placeholder="Type your message"
+            onChange={(e) => setNewMessage(e.target.value)}
+            value={newMessage}
+          />
           <button type="submit">
             <PaperPlaneRight />
           </button>
