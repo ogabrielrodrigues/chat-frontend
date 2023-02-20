eac38c4ceac46607775a2824459d3740ab565714
ogabrielrodrigues <gabrielrodrigues.dev409@gmail.com>
2023-02-20 20:59:21 -0300

    chore: removed sign page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Chat } from "./pages/Chat";
-import { Sign } from "./pages/Sign";
 
 function App() {
   const routes = createBrowserRouter([
     { path: "/chat/:room/:user", element: <Chat /> },
-    { path: "/sign", element: <Sign /> },
   ]);
 
   return <RouterProvider router={routes}></RouterProvider>;
