5b827d906e9a44f80dd3de03d68ee97dadd2fdc3
ogabrielrodrigues <gabrielrodrigues.dev409@gmail.com>
2023-02-21 18:40:20 -0300

    feat: removed unused declarations

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { SocketContextProvider } from "./contexts/SocketContext";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <SocketContextProvider>
-      <App />
-    </SocketContextProvider>
+    <App />
   </React.StrictMode>
 );
