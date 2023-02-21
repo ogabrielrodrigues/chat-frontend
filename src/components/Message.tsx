9a0c3ec01d73098e11789e6b862ae38513ce30f4
ogabrielrodrigues <gabrielrodrigues.dev409@gmail.com>
2023-02-21 18:42:40 -0300

    refactor: refactored user data types

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,27 +1,21 @@
+import { User } from "../pages/Chat";
 import "../styles/message.css";
 
 export interface MessageProps {
-  authorId: string;
-  username: string;
+  user: User;
   content: string;
   sendedAt: string;
   loggedUser: string;
 }
 
-export function Message({
-  authorId,
-  username,
-  content,
-  sendedAt,
-  loggedUser,
-}: MessageProps) {
+export function Message({ user, content, sendedAt, loggedUser }: MessageProps) {
   return (
     <div
-      className={`message ${authorId === loggedUser ? "you" : ""}`}
-      key={authorId}
+      className={`message ${user.id === loggedUser ? "you" : ""}`}
+      key={user.id}
     >
       <div className="message-top">
-        {username} - {sendedAt}
+        {user.username} - {sendedAt}
       </div>
       <div className="message-body">{content}</div>
     </div>
